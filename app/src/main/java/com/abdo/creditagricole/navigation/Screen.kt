0bd190f66075fd248eeeae05baf89be69ec3f91e
Abderrahim El Gomri <abdo.elgomri@gmail.com>
2023-05-22 12:41:53 +0200

    add a sealed class that represents all app Screens

---
+++ app/src/main/java/com/abdo/creditagricole/navigation/Screen.kt
@@ -0,0 +1,43 @@
+package com.abdo.creditagricole.navigation
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.abdo.creditagricole.R
+import com.abdo.creditagricole.navigation.account.AccountDestinations.ACCOUNTS_ROUTE
+import com.abdo.creditagricole.navigation.account.AccountDestinations.OPERATIONS_ROUTE
+
+sealed class Screen(
+    val route: String,
+    @StringRes val titleResourceId: Int,
+    @DrawableRes val iconResId: Int
+) {
+    object Accounts : Screen(
+        route = ACCOUNTS_ROUTE,
+        titleResourceId = R.string.my_accounts,
+        iconResId = R.drawable.ic_balance
+    )
+
+    object Operations : Screen(
+        route = OPERATIONS_ROUTE,
+        titleResourceId = R.string.my_account_operations,
+        iconResId = R.drawable.ic_balance
+    )
+
+    object Rib : Screen(
+        route = "rib",
+        titleResourceId = R.string.rib,
+        iconResId = R.drawable.ic_account
+    )
+
+    object Transfer : Screen(
+        route = "transfer",
+        titleResourceId = R.string.transfer,
+        iconResId = R.drawable.ic_account
+    )
+
+    object Main : Screen(
+        route = "main",
+        titleResourceId = R.string.main,
+        iconResId = R.drawable.ic_account
+    )
+}
