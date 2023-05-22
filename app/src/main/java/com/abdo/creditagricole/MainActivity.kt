3e0fb46012aee640c93d580456a7cf2c5a9a5948
Abderrahim El Gomri <abdo.elgomri@gmail.com>
2023-05-22 16:29:56 +0200

    - adapt MainNavGraph (Navhost) so that it computes its padding automatically (this is necessary for screen rotation) and to let the screens be visible on top of the BottomBar

--- app/src/main/java/com/abdo/creditagricole/MainActivity.kt
+++ app/src/main/java/com/abdo/creditagricole/MainActivity.kt
@@ -3,6 +3,10 @@ package com.abdo.creditagricole
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+import androidx.navigation.compose.rememberNavController
+import com.abdo.creditagricole.navigation.root.Graph
+import com.abdo.creditagricole.navigation.root.RootNavGraph
 import com.abdo.creditagricole.ui.theme.CreditAgricoleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,8 +14,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             CreditAgricoleTheme {
+                val navHostController = rememberNavController()
+                RootNavGraph(
+                    navHostController = navHostController,
+                    startDestination = Graph.MAIN_GRAPH
+                )
             }
         }
     }
--- app/src/main/java/com/abdo/creditagricole/navigation/main/MainNavGraph.kt
+++ app/src/main/java/com/abdo/creditagricole/navigation/main/MainNavGraph.kt
@@ -1,6 +1,16 @@
 package com.abdo.creditagricole.navigation.main
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -14,12 +24,22 @@ import com.abdo.creditagricole.presentation.transfer_feature.TransferScreen
 
 @Composable
 fun MainNavGraph(
+    paddingValues: PaddingValues,
     navHostController: NavHostController
 ) {
+    var padding by remember { mutableStateOf(PaddingValues()) }
+    padding = paddingValues
+
     NavHost(
         navController = navHostController,
         route = Graph.MAIN_GRAPH,
-        startDestination = Graph.ACCOUNT_GRAPH
+        startDestination = Graph.ACCOUNT_GRAPH,
+        modifier = Modifier
+            .padding(
+                bottom = paddingValues.calculateBottomPadding(),
+                start = padding.calculateStartPadding(LayoutDirection.Ltr),
+                end = padding.calculateEndPadding(LayoutDirection.Ltr)
+            )
     ) {
         mainRoute()
         ribRoute()
