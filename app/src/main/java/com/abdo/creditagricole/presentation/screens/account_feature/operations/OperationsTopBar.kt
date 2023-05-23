76fee6e6579ade9e831cfd2c751518dea5e74ddf
Abderrahim El Gomri <abdo.elgomri@gmail.com>
2023-05-23 18:10:48 +0200

    add OperationsTopBar

---
+++ app/src/main/java/com/abdo/creditagricole/presentation/screens/account_feature/operations/OperationsTopBar.kt
@@ -0,0 +1,64 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.abdo.creditagricole.presentation.screens.account_feature.operations
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.abdo.creditagricole.R
+
+@Composable
+fun OperationsTopBar(
+    @StringRes titleRes: Int,
+    onBackPressed: () -> Unit,
+    scrollBehavior: TopAppBarScrollBehavior
+) {
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(id = titleRes),
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    fontWeight = FontWeight.Bold
+                ),
+                textAlign = TextAlign.Center
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onBackPressed) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = stringResource(R.string.back_arrow_icon)
+                )
+            }
+        },
+    )
+}
+
+@Preview
+@Composable
+fun OperationsTopBarPreview() {
+    OperationsTopBar(
+        titleRes = R.string.my_account_operations,
+        onBackPressed = {},
+        scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    )
+}
