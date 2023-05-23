78625db3fb97f57f9a34340a9fa62e92204fa52d
Abderrahim El Gomri <abdo.elgomri@gmail.com>
2023-05-23 15:08:32 +0200

    add shared ViewModel

--- app/src/main/java/com/abdo/creditagricole/presentation/screens/account_feature/SharedViewModel.kt
+++ app/src/main/java/com/abdo/creditagricole/presentation/screens/account_feature/SharedViewModel.kt
@@ -1,13 +1,60 @@
 package com.abdo.creditagricole.presentation.screens.account_feature
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.abdo.creditagricole.data.repository.BanksRepository
+import com.abdo.creditagricole.data.source.network.mappers.toBanksUiDataMap
+import com.abdo.creditagricole.presentation.models.AccountType
+import com.abdo.creditagricole.presentation.models.BanksUiData
+import com.abdo.creditagricole.presentation.models.OperationUiData
+import com.abdo.creditagricole.presentation.screens.account_feature.operations.OperationsScreenUiState
+import com.abdo.creditagricole.util.RequestState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+typealias AccountsRequestState = RequestState<Map<AccountType, List<BanksUiData>>>
 
 @HiltViewModel
 class SharedViewModel @Inject constructor(
     private val repository: BanksRepository
 ) : ViewModel() {
+    var accountsRequestState: AccountsRequestState by mutableStateOf(RequestState.Idle)
+        private set
+
+    var operationsScreenUiState: OperationsScreenUiState by mutableStateOf(OperationsScreenUiState())
+        private set
+
+    fun updateOperationsUiData(operationsUiData: OperationUiData) {
+        operationsScreenUiState = operationsScreenUiState.copy(
+            uiData = operationsUiData
+        )
+    }
+
+    init {
+        getBanks()
+    }
+
+    private fun getBanks() {
+        accountsRequestState = RequestState.Loading
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = repository.getUserBanks()
+            when (result) {
+                is RequestState.Error -> {
+                    accountsRequestState = result
+                }
+
+                is RequestState.Success -> {
+                    accountsRequestState = RequestState.Success(result.data.toBanksUiDataMap())
+                }
+
+                else -> {}
+            }
+        }
+    }
 
 }
---
+++ app/src/main/java/com/abdo/creditagricole/presentation/screens/account_feature/operations/OperationsScreenUiState.kt
@@ -0,0 +1,15 @@
+package com.abdo.creditagricole.presentation.screens.account_feature.operations
+
+import com.abdo.creditagricole.presentation.models.OperationUiData
+
+/**
+ * UiState for the Operations screen.
+ */
+data class OperationsScreenUiState(
+    val uiData: OperationUiData = OperationUiData(
+        balance = 0.0,
+        currency = "",
+        holderName = "",
+        operations = mapOf()
+    )
+)
