a52d86a873b852d4b2871d1cd608d0cf156858cb
Abderrahim El Gomri <abdo.elgomri@gmail.com>
2023-05-23 15:14:58 +0200

    add AccountsScreen Scaffold and apply topbar with empty page

--- app/src/main/java/com/abdo/creditagricole/presentation/screens/account_feature/accounts/AccountsScreen.kt
+++ app/src/main/java/com/abdo/creditagricole/presentation/screens/account_feature/accounts/AccountsScreen.kt
@@ -1,9 +1,24 @@
 package com.abdo.creditagricole.presentation.screens.account_feature.accounts
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import com.abdo.creditagricole.navigation.Screen
+import com.abdo.creditagricole.presentation.components.EmptyPage
+import com.abdo.creditagricole.presentation.models.OperationUiData
+import com.abdo.creditagricole.presentation.screens.account_feature.AccountsRequestState
 
 @Composable
 fun AccountsScreen(
+    navigateToOperations: (OperationUiData) -> Unit,
+    accountsScreenUiState: AccountsRequestState
 ) {
-
+    Scaffold(
+        topBar = {
+            AccountsTopBar(
+                titleRes = Screen.Accounts.titleResourceId,
+            )
+        }
+    ) {
+        EmptyPage()
+    }
 }
