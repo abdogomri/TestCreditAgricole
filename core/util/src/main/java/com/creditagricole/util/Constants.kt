9111aae52d16e028847d9257b7a8bcc0acd13220
Abderrahim El Gomri <abdo.elgomri@gmail.com>
2023-05-25 20:05:11 +0100

    add SSL Pinning

--- core/util/src/main/java/com/creditagricole/util/Constants.kt
+++ core/util/src/main/java/com/creditagricole/util/Constants.kt
@@ -1,7 +1,5 @@
 package com.creditagricole.util
 
-import com.creditagricole.util.model.RequestState
-
 object Constants {
     const val BASE_URL = "https://cdf-test-mobile-default-rtdb.europe-west1.firebasedatabase.app/"
     const val USER_BANKS_END_POINT = "banks.json"
